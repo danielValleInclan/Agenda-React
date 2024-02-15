42e55804d1d2043067a6958771a0816ec395ec99
danielrodriguez <daniel.rodriguezvilches@iesvalleinclan.es>
2024-02-15 10:28:40 +0100

    Generado poryecto agenda microservicio springboot

---
+++ agenda/src/test/java/com/agendareact/agenda/AgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agendareact.agenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
