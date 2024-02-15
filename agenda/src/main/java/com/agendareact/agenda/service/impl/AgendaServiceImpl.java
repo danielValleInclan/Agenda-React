b94857d599aba906193beaf394435485d752cacd
danielrodriguez <daniel.rodriguezvilches@iesvalleinclan.es>
2024-02-15 12:08:39 +0100

    mod application.yml, service puesto en  AgendaServiceImpl, todo funciona

--- agenda/src/main/java/com/agendareact/agenda/service/impl/AgendaServiceImpl.java
+++ agenda/src/main/java/com/agendareact/agenda/service/impl/AgendaServiceImpl.java
@@ -5,10 +5,12 @@ import com.agendareact.agenda.repository.AgendaRepository;
 import com.agendareact.agenda.service.AgendaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class AgendaServiceImpl implements AgendaService {
 
     @Autowired
