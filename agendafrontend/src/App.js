6c77d46ea45497774786821932a3464d4cec0161
danielrodriguez <daniel.rodriguezvilches@iesvalleinclan.es>
2024-02-16 12:07:05 +0100

    Lista de Personas terminada, remove all funciona correctamente

--- agendafrontend/src/App.js
+++ agendafrontend/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Link as ReactRouterLink, Route, Routes } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import AgendaList from './component/agenda-list.component';
@@ -9,33 +8,35 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 class App extends Component{
   render() {
     return (
-      <div>
-        <nav className='navbar navbar-expand navbar-dark bg-dark'>
-          <Link to={"/agenda"} className="navbar-brand">
-            Agenda
-          </Link>
-          <div className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <Link to={"/agenda"} className="nav-link">
-                Persons
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to={"/add"} className="nav-link">
-                Add
-              </Link>
-            </li>
+      <Router>
+        <div>
+          <nav className='navbar navbar-expand navbar-dark bg-dark'>
+            <Link to={"/agenda"} className="navbar-brand">
+              Agenda
+            </Link>
+            <div className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link to={"/agenda"} className="nav-link">
+                  Agenda
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to={"/add"} className="nav-link">
+                  Add
+                </Link>
+              </li>
+            </div>
+          </nav>
+          <div className="container mt-3">
+            <Switch>
+            {/*El en switch se renderizarán todas los compoentes cuta URL coicidan con la activa*/}
+              <Route exact path={["/", "/agenda"]} component={AgendaList} />
+            {/*  <Route exact path="/add" component={AddTutorial} /> */}
+            {/*  <Route path="/tutorials/:id" component={Tutorial} /> */}
+            </Switch>
           </div>
-        </nav>
-        <div className="container mt-3">
-          <Switch>
-          {/*El en switch se renderizarán todas los compoentes cuta URL coicidan con la activa*/}
-            <Route exact path={["/", "/agenda"]} component={AgendaList} />
-          {/*  <Route exact path="/add" component={AddTutorial} /> */}
-          {/*  <Route path="/tutorials/:id" component={Tutorial} /> */}
-          </Switch>
         </div>
-      </div>
+      </Router>
     )
   }
 }
--- agendafrontend/src/component/agenda-list.component.js
+++ agendafrontend/src/component/agenda-list.component.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import AgendaDataService from "../services/agenda.service";
-import { Link } from "react-router-dom";
+import { Link, BrowserRouter as Router } from "react-router-dom"; 
 
 export default class AgendaList extends Component {
     constructor(props) {
@@ -88,87 +88,114 @@ export default class AgendaList extends Component {
         const { searchName, persons, currentPerson, currentIndex } = this.state;
         //ponemos los distintos elementos del estado en variables para simplificar su acceso dentro del método
         return (
-          <div className="list row">
-            <div className="col-md-8">
-              <div className="input-group mb-3">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Search by title"
-                  value={searchName}
-                  onChange={this.onChangeSearchName}
-                />
-                <div className="input-group-append">
-                  <button
-                    className="btn btn-outline-secondary"
-                    type="button"
-                    onClick={this.searchName}
-                  >
-                    Search
-                  </button>
+          <Router>
+            <div className="list row">
+              <div className="col-md-8">
+                <div className="input-group mb-3">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Search by name"
+                    value={searchName}
+                    onChange={this.onChangeSearchName}
+                  />
+                  <div className="input-group-append">
+                    <button
+                      className="btn btn-outline-secondary"
+                      type="button"
+                      onClick={this.searchName}
+                    >
+                      Search
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="col-md-6">
-              <h4>Persons List</h4>
-    
-              <ul className="list-group">
-                {/*El operedor && lógico. Los dos elementos tienen que ser true, en este caso no vacio, para que se ejecute la sentencia */}
-                {/*si tutorials está vacio , no se ejecuta el map*/}
-    
-                {persons &&
-                  persons.map((person, index) => (
-                    <li
-                  /* Cambiamos la clase del elemento de la lista seleccionado. Ponemos fondo azul*/
-                      className={
-                        "list-group-item " +
-                        (index === currentIndex ? "active" : "")
-                      }
-                      onClick={() => this.setActivePerson(person, index)}
-                      key={index}
-                    >
-                      {person.name}
-                    </li>
-                  ))}
-              </ul>
-    
-              <button
-                className="m-3 btn btn-sm btn-danger"
-                onClick={this.removeAllPersons}
-              >
-                Remove All
-              </button>
-            </div>
-            <div className="col-md-6">
-              {/*Renderizado condicional. Si current tutorial el null se dibuja lo de abajo. Si no,*/}
-              {/*se dibuja "Please click on a Tutorial..." ver más abajo.*/}
-              {currentPerson ? (
-                <div>
-                  <h4>Person</h4>
+              <div className="col-md-6">
+                <h4>Persons List</h4>
+      
+                <ul className="list-group">
+                  {/*El operedor && lógico. Los dos elementos tienen que ser true, en este caso no vacio, para que se ejecute la sentencia */}
+                  {/*si tutorials está vacio , no se ejecuta el map*/}
+      
+                  {persons &&
+                    persons.map((person, index) => (
+                      <li
+                    /* Cambiamos la clase del elemento de la lista seleccionado. Ponemos fondo azul*/
+                        className={
+                          "list-group-item " +
+                          (index === currentIndex ? "active" : "")
+                        }
+                        onClick={() => this.setActivePerson(person, index)}
+                        key={index}
+                      >
+                        {person.firstName + " " + person.lastName}
+                      </li>
+                    ))}
+                </ul>
+      
+                <button
+                  className="m-3 btn btn-sm btn-danger"
+                  onClick={this.removeAllPersons}
+                >
+                  Remove All
+                </button>
+              </div>
+              <div className="col-md-6">
+                {/*Renderizado condicional. Si current tutorial el null se dibuja lo de abajo. Si no,*/}
+                {/*se dibuja "Please click on a Tutorial..." ver más abajo.*/}
+                {currentPerson ? (
                   <div>
-                    <label>
-                      <strong>Name:</strong>
-                    </label>{" "}
-                    {currentPerson.name}
+                    <h4>Person</h4>
+                    <div>
+                      <label>
+                        <strong>Name:</strong>
+                      </label>{" "}
+                      {currentPerson.firstName}
+                      <br/>
+                      <label>
+                        <strong>LastName:</strong>
+                      </label>{" "}
+                      {currentPerson.lastName}
+                      <br/>
+                      <label>
+                        <strong>Street:</strong>
+                      </label>{" "}
+                      {currentPerson.street}
+                      <br/>
+                      <label>
+                        <strong>PostalCode:</strong>
+                      </label>{" "}
+                      {currentPerson.postalCode}
+                      <br/>
+                      <label>
+                        <strong>City:</strong>
+                      </label>{" "}
+                      {currentPerson.city}
+                      <br/>
+                      <label>
+                        <strong>Birthday:</strong>
+                      </label>{" "}
+                      {currentPerson.birthday}
+                    </div>
+      
+                    <Link
+                      //Como hemos incluido en el switch esta ruta, /tutorials/+id se renderizará el componente
+                      // tutorials cuando se pulse el enlace.
+                      to={"/agenda/" + currentPerson.id}
+                      className="badge badge-warning"
+                    >
+                      Edit
+                    </Link>
                   </div>
-    
-                  <Link
-                    //Como hemos incluido en el switch esta ruta, /tutorials/+id se renderizará el componente
-                    // tutorials cuando se pulse el enlace.
-                    to={"/agenda/" + currentPerson.id}
-                    className="badge badge-warning"
-                  >
-                    Edit
-                  </Link>
-                </div>
-              ) : (
-                <div>
-                  <br />
-                  <p>Please click on a Person...</p>
-                </div>
-              )}
+                ) : (
+                  <div>
+                    <br />
+                    <p>Please click on a Person...</p>
+                  </div>
+                )}
+              </div>
             </div>
-          </div>
+          </Router>
         );
       }
 
