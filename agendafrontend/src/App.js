21c6a70d60418e602e51cecf52a127959b238f2b
danielrodriguez <daniel.rodriguezvilches@iesvalleinclan.es>
2024-02-16 10:19:51 +0100

    app.js mod para agenda

--- agendafrontend/src/App.js
+++ agendafrontend/src/App.js
@@ -1,6 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Link as ReactRouterLink, Route, Routes } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import AgendaList from './component/agenda-list.component';
 
+
+class App extends Component{
+  render() {
+    return (
+      <div>
+        <nav className='navbar navbar-expand navbar-dark bg-dark'>
+          <Link to={"/agenda"} className="navbar-brand">
+            Agenda
+          </Link>
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/agenda"} className="nav-link">
+                Persons
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/add"} className="nav-link">
+                Add
+              </Link>
+            </li>
+          </div>
+        </nav>
+        <div className="container mt-3">
+          <Switch>
+          {/*El en switch se renderizarán todas los compoentes cuta URL coicidan con la activa*/}
+            <Route exact path={["/", "/agenda"]} component={AgendaList} />
+          {/*  <Route exact path="/add" component={AddTutorial} /> */}
+          {/*  <Route path="/tutorials/:id" component={Tutorial} /> */}
+          </Switch>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
+
+/*
 function App() {
   return (
     <div className="App">
@@ -23,3 +64,4 @@ function App() {
 }
 
 export default App;
+*/
