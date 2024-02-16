87b0a2ee045d4a82839b4925e60163f0beeb5867
danielrodriguez <daniel.rodriguezvilches@iesvalleinclan.es>
2024-02-16 10:13:21 +0100

    agenda-list.component.js creado

---
+++ agendafrontend/src/component/agenda-list.component.js
@@ -0,0 +1,176 @@
+import React, { Component } from "react";
+import TutorialDataService from "../services/tutorial.service";
+import { Link } from "react-router-dom";
+
+export default class AgendaList extends Component {
+    constructor(props) {
+        super(props);
+        this.onChangeSearchName = this.onChangeSearchName.bind(this);
+        this.retrievePersons = this.retrievePersons.bind(this);
+        this.refreshList = this.refreshList.bind(this);
+        this.setActivePerson = this.setActivePerson.bind(this);
+        this.removeAllPersons = this.removeAllPersons.bind(this);
+        this.searchName = this.searchName.bind(this);
+
+        this.state = {
+            persons: [],
+            currentPerson: null,
+            currentIndex: -1,
+            searchName: ""
+        };
+    }
+
+    componentDidMount() {
+        this.retrievePersons();
+    }
+
+    onChangeSearchName(e) {
+        const searchName = e.target.value;
+
+        this.setState({
+            searchName: searchName
+        });
+    }
+
+    retrievePersons() {
+        AgendaDataService.getAll()
+            .then(response => {
+            this.setState({
+                persons: response.data
+            });
+            console.log(response.data);
+            })
+            .catch(e => {
+            console.log(e);
+            });
+    }
+
+    refreshList() {
+        this.retrievePersons();
+        this.setState({
+            currentPerson: null,
+            currentIndex: -1
+        });
+    }
+
+    setActivePerson(person, index) {
+        this.setState({
+            currentPerson: person,
+            currentIndex: index
+        });
+    }
+    
+    removeAllPersons() {
+        AgendaDataService.deleteAll()
+          .then(response => {
+            console.log(response.data);
+            this.refreshList();
+          })
+          .catch(e => {
+            console.log(e);
+          });
+    }
+
+    searchName() {
+        AgendaDataService.findByName(this.state.searchName)
+          .then(response => {
+            this.setState({
+              person: response.data
+            });
+            console.log(response.data);
+          })
+          .catch(e => {
+            console.log(e);
+          });
+    }
+
+    render() {
+        const { searchName, persons, currentPerson, currentIndex } = this.state;
+        //ponemos los distintos elementos del estado en variables para simplificar su acceso dentro del método
+        return (
+          <div className="list row">
+            <div className="col-md-8">
+              <div className="input-group mb-3">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Search by title"
+                  value={searchName}
+                  onChange={this.onChangeSearchName}
+                />
+                <div className="input-group-append">
+                  <button
+                    className="btn btn-outline-secondary"
+                    type="button"
+                    onClick={this.searchName}
+                  >
+                    Search
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <h4>Persons List</h4>
+    
+              <ul className="list-group">
+                {/*El operedor && lógico. Los dos elementos tienen que ser true, en este caso no vacio, para que se ejecute la sentencia */}
+                {/*si tutorials está vacio , no se ejecuta el map*/}
+    
+                {persons &&
+                  persons.map((person, index) => (
+                    <li
+                  /* Cambiamos la clase del elemento de la lista seleccionado. Ponemos fondo azul*/
+                      className={
+                        "list-group-item " +
+                        (index === currentIndex ? "active" : "")
+                      }
+                      onClick={() => this.setActivePerson(person, index)}
+                      key={index}
+                    >
+                      {person.name}
+                    </li>
+                  ))}
+              </ul>
+    
+              <button
+                className="m-3 btn btn-sm btn-danger"
+                onClick={this.removeAllPersons}
+              >
+                Remove All
+              </button>
+            </div>
+            <div className="col-md-6">
+              {/*Renderizado condicional. Si current tutorial el null se dibuja lo de abajo. Si no,*/}
+              {/*se dibuja "Please click on a Tutorial..." ver más abajo.*/}
+              {currentPerson ? (
+                <div>
+                  <h4>Person</h4>
+                  <div>
+                    <label>
+                      <strong>Name:</strong>
+                    </label>{" "}
+                    {currentPerson.name}
+                  </div>
+    
+                  <Link
+                    //Como hemos incluido en el switch esta ruta, /tutorials/+id se renderizará el componente
+                    // tutorials cuando se pulse el enlace.
+                    to={"/agenda/" + currentPerson.id}
+                    className="badge badge-warning"
+                  >
+                    Edit
+                  </Link>
+                </div>
+              ) : (
+                <div>
+                  <br />
+                  <p>Please click on a Person...</p>
+                </div>
+              )}
+            </div>
+          </div>
+        );
+      }
+
+
+}
